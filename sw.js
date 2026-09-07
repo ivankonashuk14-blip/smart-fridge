eaea66d69cf7185c866383d67568358b60ba6026
ivankonashuk14-blip <ivankonashuk14@gmail.com>
2026-09-08 00:03:43 +0300

    Add files via upload
    Обновление сайта и иконки

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'smart-fridge-shell-v1';
-const APP_SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+const APP_SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg', '/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
